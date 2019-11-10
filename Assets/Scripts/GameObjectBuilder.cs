a9039ddd0dbc7acb75b71e97b2489d0ea66324be
Den <surin.denis.k@gmail.com>
2019-11-10 15:48:56 +0300

    lesson 6

---
+++ Assets/Scripts/GameObjectBuilder.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameObjectBuilder : MonoBehaviour
+{
+	public static GameObjectBuilder Instance;
+
+	public GameObject StandardTurret;
+
+	static GameObjectBuilder()
+	{
+		Instance = new GameObjectBuilder();
+	}
+	
+	private void Awake()
+	{
+		if (GameObjectBuilder.Instance == null)
+		{
+			Debug.LogWarning("static ctor did not work for Instance");
+			GameObjectBuilder.Instance = this;
+		}
+	}
+	
+	public GameObject GetNewTurret(Vector3 position, Quaternion rotation)
+	{
+		return (GameObject)Instantiate(StandardTurret, position, rotation);
+	}
+
+	void Start()
+	{
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Platform.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+public class Platform : MonoBehaviour
+{	
+	private GameObject Turet;
+	private Renderer Rend;
+	private Color StartColor;
+
+	public Vector3 TurretOffset;
+	public Color SelectPlatfomColor;
+
+	private void Awake()
+	{
+		Rend = GetComponent<Renderer>();
+		StartColor = Rend.material.color;
+	}	
+
+	private void OnMouseUp()
+	{
+		if (Turet != null)
+		{
+			Debug.Log("Can not build here");
+			return;
+		}
+		Turet = GameObjectBuilder.Instance.GetNewTurret(transform.position + TurretOffset, transform.rotation);
+	}
+
+	private void OnMouseEnter()
+	{
+		Rend.material.color = SelectPlatfomColor;
+	}
+
+	private void OnMouseExit()
+	{
+		Rend.material.color = StartColor;
+	}
+
+}
